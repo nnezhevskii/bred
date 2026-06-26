66852fc6abeeefcd2366de4811643dd86e90f2c4
n.nezhevskii <empthynickname@gmail.com>
2026-06-26 20:45:37 +0200

    c transpiler

---
+++ src/test/kotlin/org/nnezh/lltag/SnapshotGenerator.kt
@@ -0,0 +1,29 @@
+package org.nnezh.lltag
+
+import org.nnezh.org.nnezh.ICGenerator.PrettyPrinter
+import org.nnezh.org.nnezh.compiler.TACCompilerImpl
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main() {
+    listOf(
+        "factorial",
+        "for",
+        "sortThree",
+        "isEven",
+        "complexShock",
+        "isPositive",
+        "minValue",
+        "random_case_failed_for_no_reason",
+    ).forEach { testName ->
+        val sourcePath = Paths.get("src/test/resources/$testName.bred")
+        val outputPath = Paths.get("src/test/resources/$testName.3ac")
+        val src = Files.readString(sourcePath)
+        val actual = TACCompilerImpl()
+            .compile(src)
+            .let { PrettyPrinter().format(it) }
+            .joinToString("\n")
+        Files.writeString(outputPath, actual)
+        println("Generated $outputPath")
+    }
+}
